08bbdd8da699916cded50f6107e04f7f67e80e86
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-02-01 21:57:23 +0800

    fix: remove prettier from devDependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css'
-
-import ContentEditor from './components/ContentEditor'
-import Card from './components/Card'
-import DummyAvatar from './assets/images/avatars/image-juliusomo.webp'
+import "./App.css";
+import ContentEditor from "./components/ContentEditor";
+import Card from "./components/Card";
+import DummyAvatar from "./assets/images/avatars/image-juliusomo.webp";
 
 function App() {
   return (
     <div className="App">
       <Card>
         <ContentEditor />
-        <div className='contentContainer'>
-          <img className='avatar' src={DummyAvatar} />
-          <button className='buttonPrimary'>Send</button>
+        <div className="contentContainer">
+          <img className="avatar" src={DummyAvatar} />
+          <button className="buttonPrimary">Send</button>
         </div>
       </Card>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
