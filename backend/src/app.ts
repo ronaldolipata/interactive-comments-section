a98ed83739916bb52f8924b7577dfbdc05ed4bb9
antonpalermo <ANPBusinessmail@gmail.com>
2023-02-02 22:04:56 +0800

    feat: init prisma

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,29 @@
+import "dotenv/config"
+
 import express, { Request, Response } from "express"
+import { PrismaClient } from "@prisma/client"
 
 async function main() {
   const app = express()
+  const port = process.env.PORT || 4000
+
+  const prisma = new PrismaClient()
 
   app.get("/", (req: Request, res: Response) => {
     res.status(200).json({ message: "Server OK" })
   })
 
-  app.listen(4000, () => {
+  app.post("/create", async (req: Request, res: Response) => {
+    const user = await prisma.user.create({
+      data: {
+        username: "antonpalermoo",
+      },
+    })
+
+    return res.status(201).json(user)
+  })
+
+  app.listen(port, () => {
     console.log("Server Started")
   })
 }
