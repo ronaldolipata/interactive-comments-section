53b715f96ef8c6b8bb811d73a673c8b25b165b82
antonpalermo <ANPBusinessmail@gmail.com>
2023-02-04 22:27:50 +0800

    fix: server errors

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,17 @@
 # Commenter Backend API
+
+## Running on you machine
+
+To run this server on your local machine you need to cd to backend first then run
+
+```
+npm install
+```
+
+once all dependecies are installed you can now run
+
+```
+npm run dev
+```
+
+this will start the server on port 4000
--- backend/src/controllers/interactions/interact.ts
+++ backend/src/controllers/interactions/interact.ts
@@ -1,27 +1,19 @@
-import { Response } from "express"
-import { Interaction, Prisma, PrismaClient } from "@prisma/client"
+import { Response } from "express";
+import { ActionType, Interaction, Prisma, PrismaClient } from "@prisma/client";
 
-import prisma from "../../utils/prisma"
-import IncomingRequest from "../../utils/requests"
+import IncomingRequest from "../../utils/requests";
 
-type InteractionPayload = Pick<Interaction, "type" | "content"> & {}
+type InteractionPayload = Pick<Interaction, "type" | "content"> & {};
 
-export default async function interact(
+export default function interact(
   req: IncomingRequest<InteractionPayload>,
   res: Response
 ) {
-  const { type, content } = req.body
+  const { type, content } = req.body;
+  const prisma = new PrismaClient();
 
-  try {
-    const interaction = await prisma.interaction.create({
-      data: { type, content },
-    })
-
-    return res.status(201).json(interaction)
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("ECODE: ", error.code, "EMSG: ", error.message)
-    }
-    return res.status(500).end()
-  }
+  return prisma.interaction
+    .create({ data: { type, content } })
+    .then((result) => res.status(201).json(result))
+    .catch((e) => res.status(400).json({ error: "Error interact", stack: e }));
 }
