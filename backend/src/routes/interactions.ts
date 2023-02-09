36c4ca3453696ed52bc7b8c90d1bdabb22ce1395
antonpalermo <ANPBusinessmail@gmail.com>
2023-02-09 21:31:01 +0800

    feat: added downvote functionality
    added downvote query to use this just send a post request to
    ".../downvote/:id", headers "application/json" and body { count: 1 }
    this will deduct to the current upvote count.

--- backend/src/routes/interactions.ts
+++ backend/src/routes/interactions.ts
@@ -2,11 +2,14 @@ import express from 'express'
 
 import upvote from '../controllers/interactions/upvote'
 import comment from '../controllers/interactions/comment'
+import downvote from '../controllers/interactions/downvote'
 
 const router = express.Router()
 
 router.post('/comment', comment)
 
 router.post('/upvote/:id', upvote)
+
+router.post('/downvote/:id', downvote)
 
 export default router
