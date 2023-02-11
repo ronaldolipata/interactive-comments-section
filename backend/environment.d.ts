fa4bdd19f3a5c2417b768ef772de6cb1e905c31c
antonpalermo <ANPBusinessmail@gmail.com>
2023-02-11 21:16:09 +0800

    feat: added bcrypt to hash passwords

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: "development" | "production"
       FONTEND_URL: string
+      HASH_SECRET: string
       DATABASE_URL: string
       PORT?: number
     }
--- backend/src/controllers/users/create.ts
+++ backend/src/controllers/users/create.ts
@@ -1,4 +1,5 @@
 import { Response } from 'express'
+import { hash } from 'bcrypt'
 
 import UserSchema from '../../models/users'
 import IncomingRequest from '../../utils/requests'
@@ -15,10 +16,22 @@ export default async function handler(
 ) {
   const { email, username, password } = req.body
 
-  try {
-    const createdUser = await UserSchema.create({ username, password, email })
+  const hashPassword = await hash(password, 10)
 
-    return res.status(200).json(createdUser)
+  try {
+    const createdUser = await UserSchema.create({
+      username,
+      password: hashPassword,
+      email
+    })
+
+    return res.status(200).json({
+      _id: createdUser.id,
+      username: createdUser.username,
+      email: createdUser.email,
+      createdAt: createdUser.createdAt,
+      updatedAt: createdUser.updatedAt
+    })
   } catch (error) {
     console.log(error)
     return res.status(400).end()
