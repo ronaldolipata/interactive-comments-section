d00b110e53b292cb1d39161701cfe5ebac074351
antonpalermo <ANPBusinessmail@gmail.com>
2023-03-04 09:57:45 +0800

    created comments slice

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -3,15 +3,28 @@ import Comment from '../Comment';
 import CommentBox from '../CommentBox';
 import DeleteConfirmationModal from '../DeleteModal';
 import '../../styles/main.css';
+import { useDispatch } from 'react-redux';
+import { initializeComments } from '../../store/features/comments/commentsSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => dispatch(initializeComments(data.comments)));
+  }, []);
+
   // Initialize showDeleteModal state
   const showDeleteModal = useAppSelector(
     (state) => state.deleteModal.showDeleteModal
   );
 
+  const comments = useAppSelector((state) => state.commentsSlice.comments);
+
   return (
-    <div className='container'>
+    <div className="container">
       <Comment />
       <CommentBox />
       {showDeleteModal && <DeleteConfirmationModal />}
